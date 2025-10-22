61d13acdaf82579c758e889e6a742f24a0ad481c
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-10-21 19:28:22 -0500

    switch to state-based system

---
+++ level.lua
@@ -0,0 +1,130 @@
+local lvl={}
+
+local function fallBlocks(map)
+    for y=#map,1,-1 do
+        for x=1,#map[y] do
+            if y+1<=#map and map[y+1][x]==0 then
+                local tile=map[y][x]
+                map[y][x]=0
+                map[y+1][x]=tile
+
+                local oTile=map[y+1][x]
+            end
+        end
+    end
+end
+
+local function rmvBlocks(map)
+    for y=#map,1,-1 do
+        for x=1,#map[y] do
+            local oTile=map[y][x]
+            if oTile~=0 then
+
+                --y <;3
+                local num=1
+
+                local yy = y
+
+                while yy<#map and map[yy+1][x]==oTile do
+                    yy=yy+1
+                    num=num+1
+                end
+
+                if num>2 then
+                    for i=y-1,y+num-1 do
+                        map[i][x]=0
+                    end
+                end
+                
+                --x >:3zx
+                local num=1
+
+                local xx = x
+
+                while xx<#map[y] and map[y][xx+1]==oTile do
+                    xx=xx+1
+                    num=num+1
+                end
+
+                if num>2 then
+                    for i=x-1,x+num-1 do
+                        map[y][i]=0
+                    end
+                end
+            end
+        end
+    end
+end
+
+local function checkBlockFall(map)
+    for y=#map,1,-1 do
+        for x=1,#map[y] do
+            if (map[y][x]~=0 and y<#map) and (map[y+1] and map[y+1][x]==0) then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function lvl:init()
+    self.bricks={lg.newImage("assets/brick1.png"),lg.newImage("assets/brick2.png")}
+    self.fruitImg=lg.newImage("assets/fruit.png")
+    self.fruitQuads={}
+    for x=0,self.fruitImg:getWidth()/8 - 1 do
+        local quad=love.graphics.newQuad(x*8,0,8,8,self.fruitImg:getWidth(),self.fruitImg:getHeight())
+        table.insert(self.fruitQuads,quad)
+    end
+end
+
+function lvl:enter()
+    self.time=0
+    self.map=generateMap(10,10,0)
+
+    for x=1,10 do
+        for y=1,10 do
+            if math.random(0,10)==1 then
+                self.map[y][x]=math.random(1,4)
+            end
+        end
+    end
+end
+
+function lvl:update(dt)
+    self.time=self.time+dt
+
+    if self.time>=0.25 then
+        if not checkBlockFall(self.map) then
+            rmvBlocks(self.map)
+            self.map[1][math.random(1,10)]=math.random(1,4)
+        end
+        fallBlocks(self.map)
+        
+        self.time=0
+    end
+end
+
+function lvl:draw()
+    shove.beginDraw()
+        shove.beginLayer("game")
+            for x=0,9 do
+                for y=0,9 do
+                    if (x+y)%2==1 then
+                        lg.draw(self.bricks[1],x*8,y*8)
+                    else
+                        lg.draw(self.bricks[2],x*8,y*8)
+                    end
+                end
+            end
+            for y,v in ipairs(self.map) do
+                for x,j in ipairs(v) do
+                    if j~=0 then
+                        lg.draw(self.fruitImg,self.fruitQuads[j],(x-1)*8,(y-1)*8)
+                    end
+                end
+            end
+        shove.endLayer()
+    shove.endDraw()
+end
+
+return lvl
--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 require("init")
 lg=love.graphics
+lm=love.math
+la=love.audio
 
 function generateMap(w,h,val)
     local temp={}
@@ -13,142 +15,28 @@ function generateMap(w,h,val)
 end
 
 function love.load()
+
+    gs=require("lib.hump.gamestate")
+    gs.registerEvents()
+
+    state={
+        ["level"] = require("level")
+    }
+    
     math.randomseed(love.math.random(0,845261654))
     rotate=require("lib.rotate")
     shove.createLayer("game")
-    map=generateMap(10,10,0)
 
-    img=lg.newImage("assets/fruit.png")
-    quads={}
-    for x=0,img:getWidth()/8 - 1 do
-        local quad=love.graphics.newQuad(x*8,0,8,8,img:getWidth(),img:getHeight())
-        table.insert(quads,quad)
-    end
-    map[1][1]=1
-    map[2][1]=1
-    map[3][1]=1
-    map[4][1]=1
+    gs.switch(state["level"])
 
-
-    --[[for x=1,10 do
-        for y=1,10 do
-            if math.random(0,10)==1 then
-                map[y][x]=math.random(1,3)
-            end
-        end
-    end]]
-
-    --[[for i=1,3 do
-        for j=1,3 do
-            local x,y=math.random(1,10),math.random(1,10)
-            if map[y][x]==0 then
-                map[y][x]=i
-            end
-        end
-    end]]
-
-    time=0
-end
-
-function fallBlocks()
-    for y=#map,1,-1 do
-        for x=1,#map[y] do
-            if y+1<=#map and map[y+1][x]==0 then
-                local tile=map[y][x]
-                map[y][x]=0
-                map[y+1][x]=tile
-
-                local oTile=map[y+1][x]
-            end
-        end
-    end
-end
-
-function rmvBlocks()
-    for y=#map,1,-1 do
-        for x=1,#map[y] do
-            local oTile=map[y][x]
-            if oTile~=0 then
-
-                --y <;3
-                local num=1
-
-                local yy = y
-
-                while yy<#map and map[yy+1][x]==oTile do
-                    yy=yy+1
-                    num=num+1
-                end
-
-                if num>2 then
-                    for i=y-1,y+num-1 do
-                        map[i][x]=0
-                    end
-                end
-                
-                --x >:3zx
-                local num=1
-
-                local xx = x
-
-                while xx<#map[y] and map[y][xx+1]==oTile do
-                    xx=xx+1
-                    num=num+1
-                end
-
-                if num>2 then
-                    for i=x-1,x+num-1 do
-                        map[y][i]=0
-                    end
-                end
-            end
-        end
-    end
-end
-
-function checkBlockFall()
-    for y=#map,1,-1 do
-        for x=1,#map[y] do
-            if (map[y][x]~=0 and y<#map) and (map[y+1] and map[y+1][x]==0) then
-                return true
-            end
-        end
-    end
-    return false
 end
 
 function love.update(dt)
     input:update()
-
-    time=time+dt
-
-    if time>=0.4 then
-        if not checkBlockFall() then
-            rmvBlocks()
-            map[1][math.random(1,10)]=math.random(1,3)
-        end
-        fallBlocks()
-        
-        time=0
-    end
 end 
 
 function love.draw()
-    shove.beginDraw()
-        shove.beginLayer("game")
-            lg.setColor(0.4,0.4,0.4)
-            lg.rectangle("fill",0,0,80,80)
-            lg.setColor(1,1,1,1)
-            for y,v in ipairs(map) do
-                for x,j in ipairs(v) do
-                    if j~=0 then
-                        lg.draw(img,quads[j],(x-1)*8,(y-1)*8)
-                    end
-                end
-            end
-        shove.endLayer()
-    shove.endDraw()
-    lg.print(tostring(checkBlockFall()))
+    
 end
 
 function love.keypressed(k)
