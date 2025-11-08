3cdd5824fd2a37796837c7a879247a8fc51674cf
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-08 13:37:11 -0600

    add build data

---
+++ build.lua
@@ -0,0 +1,9 @@
+return {
+      name = 'FruitSpin',
+      config = 'nonsteam',
+      version = '1.0',
+      love = '11.5',
+      icon = 'assets/icon.png',
+      platforms = {'windows','linux','macos'},
+      ignore = {}
+}
