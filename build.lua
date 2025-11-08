f322641b2e6a8df235b19dd4ea2f58bac81fd569
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-11-08 13:48:04 -0600

    update build.lua

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 return {
       name = 'FruitSpin',
       config = 'nonsteam',
-      version = '1.0',
+      version = '1.1',
       love = '11.5',
       icon = 'assets/icon.png',
       platforms = {'windows','linux','macos'},
